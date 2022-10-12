3bb8b64d629d7d33276147dd586200c94eb08bc7
Victor Petrunin <petruninviktor9464@gmail.com>
2022-10-12 09:09:04 +0300

    first commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,14 @@
+public class Post {
+
+    public String FormDate;
+    public String name;
+
+    public String patronymic;
+    public String phone;
+    public String passport;
+    public String surname;
+    public boolean subscription;
+
+    public FormDate birthday;
+
+}
